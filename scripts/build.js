19f7b3800d2be0b532cfe45bbf2f9934a91f436e
mvc_ydb_admin <1609059419@qq.com>
2024-02-28 19:32:10 +0800

    chore: delete scripts

--- scripts/build.js
+++
@@ -1,7 +0,0 @@
-const { readdirSync } = require("fs");
-const checkAndBuild = require("./check-deps-and-build");
-
-(() => {
-  const allPkgs = readdirSync("packages");
-  checkAndBuild(allPkgs);
-})();
